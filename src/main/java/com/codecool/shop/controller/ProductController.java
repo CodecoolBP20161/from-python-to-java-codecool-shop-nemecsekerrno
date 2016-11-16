102ae30eb0b6bb21cd41e247ee438ba317bf5b4e
rekan <rekanagy06@gmail.com>
2016-11-16 10:12:49 +0100

    Refactor filter handler, create supplier filter

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,9 +2,13 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 import spark.Request;
 import spark.Response;
 import spark.ModelAndView;
@@ -14,27 +18,43 @@ import java.util.Map;
 
 public class ProductController {
 
+    private static Map params = new HashMap<>();
+
     public static ModelAndView renderProducts(Request req, Response res) {
-        return categoryFilterHandler(0);
+        filterHandler();
+        return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductsByCategory(Request req, Response res) {
-        int categoryId = Integer.parseInt(req.params(":categoryId"));
-        return categoryFilterHandler(categoryId);
+        filterHandler(ProductCategoryDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        return new ModelAndView(params, "product/index");
     }
 
-    private static ModelAndView categoryFilterHandler(int categoryId) {
+    public static ModelAndView renderProductsBySupplier(Request req, Response res) {
+        filterHandler(SupplierDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        return new ModelAndView(params, "product/index");
+    }
+
+    private static void filterHandler() {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
-        Map params = new HashMap<>();
         params.put("categories", productCategoryDataStore.getAll());
-        if (categoryId == 0) {
-            params.put("products", productDataStore.getAll());
-        } else {
-            params.put("currCategory", productCategoryDataStore.find(categoryId));
-            params.put("products", productDataStore.getBy(productCategoryDataStore.find(categoryId)));
-        }
-        return new ModelAndView(params, "product/index");
+        params.put("suppliers", supplierDataStore.getAll());
+        params.put("currFilter", null);
+        params.put("products", productDataStore.getAll());
+    }
+
+    private static void filterHandler(ProductCategory filter) {
+        filterHandler();
+        params.put("currFilter", filter);
+        params.put("products", filter.getProducts());
+    }
+
+    private static void filterHandler(Supplier filter) {
+        filterHandler();
+        params.put("currFilter", filter);
+        params.put("products", filter.getProducts());
     }
 }
