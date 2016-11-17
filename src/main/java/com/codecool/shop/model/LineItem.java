ecb0e9e39581da26d58b1dc0d9d5f5c938868f1c
rekan <rekanagy06@gmail.com>
2016-11-17 10:23:03 +0100

    Add increaseQty method

--- src/main/java/com/codecool/shop/model/LineItem.java
+++ src/main/java/com/codecool/shop/model/LineItem.java
@@ -4,14 +4,37 @@ package com.codecool.shop.model;
  * Created by berloc on 2016.11.16..
  */
 public class LineItem {
-
     private Product product;
     private Integer qty;
 
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
     public LineItem(){}
 
-    public LineItem(Product product){
-        product = product;
+    public LineItem(Product prod){
+        product = prod;
         qty = 1;
+    }
+
+    public float getTotalPrice(){
+        return product.getPriceInt()*qty;
+    }
+
+    public void increaseQty() {
+        qty += 1;
     }
 }
