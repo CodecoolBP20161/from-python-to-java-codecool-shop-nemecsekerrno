9c5fce89073b6a9f154e4d9954cf9817ff12e982
rekan <rekanagy06@gmail.com>
2016-11-17 10:27:58 +0100

    modify filterHandler to pass on Request, add handleCart

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -7,54 +7,94 @@ import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.LineItem;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.ShoppingCart;
 import com.codecool.shop.model.Supplier;
 import spark.Request;
 import spark.Response;
 import spark.ModelAndView;
 
 import java.util.HashMap;
+import java.util.IntSummaryStatistics;
 import java.util.Map;
+import java.util.StringJoiner;
 
 public class ProductController {
 
     private static Map params = new HashMap<>();
 
     public static ModelAndView renderProducts(Request req, Response res) {
-        filterHandler();
+        filterHandler(req);
+        saveLastURL(req);
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductsByCategory(Request req, Response res) {
-        filterHandler(ProductCategoryDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        filterHandler(req, ProductCategoryDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        saveLastURL(req);
         return new ModelAndView(params, "product/index");
     }
 
     public static ModelAndView renderProductsBySupplier(Request req, Response res) {
-        filterHandler(SupplierDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        filterHandler(req, SupplierDaoMem.getInstance().find(Integer.parseInt(req.params(":id"))));
+        saveLastURL(req);
         return new ModelAndView(params, "product/index");
     }
 
-    private static void filterHandler() {
+    public static ModelAndView handleAddToCart(Request req, Response res) {
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        Integer prodID = Integer.parseInt(req.params(":prodID"));
+        LineItem lineItem = new LineItem(productDataStore.find(prodID));
+        if (req.session().attribute("cart") == null) {
+            ShoppingCart cart = new ShoppingCart();
+            cart.getSessionItems().add(lineItem);
+            req.session().attribute("cart", cart);
+        } else {
+            ShoppingCart cart = req.session().attribute("cart");
+            cart.addToCart(lineItem);
+            req.session().attribute("cart", cart);
+        }
+        res.redirect(getLastURL(req));
+        return null;
+    }
+
+    private static void filterHandler(Request req) {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ShoppingCart cart = req.session().attribute("cart");
 
         params.put("categories", productCategoryDataStore.getAll());
         params.put("suppliers", supplierDataStore.getAll());
         params.put("currFilter", null);
         params.put("products", productDataStore.getAll());
+        params.put("cartItemTotal", cart != null ? cart.totalQuantity() : null);
     }
 
-    private static void filterHandler(ProductCategory filter) {
-        filterHandler();
+    private static void filterHandler(Request req, ProductCategory filter) {
+        filterHandler(req);
         params.put("currFilter", filter);
         params.put("products", filter.getProducts());
     }
 
-    private static void filterHandler(Supplier filter) {
-        filterHandler();
+    private static void filterHandler(Request req, Supplier filter) {
+        filterHandler(req);
         params.put("currFilter", filter);
         params.put("products", filter.getProducts());
+    }
+
+    private static void saveLastURL(Request req) {
+        req.session().attribute("lastURL", req.pathInfo());
+    }
+
+    private static String getLastURL(Request req) {
+        String redirectURL = "";
+        try {
+            redirectURL = req.session().attribute("lastURL");
+        } catch (NullPointerException e) {
+            redirectURL = "/";
+        }
+        return redirectURL;
     }
 }
