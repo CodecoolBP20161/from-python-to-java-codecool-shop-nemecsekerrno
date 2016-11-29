66a31847fcc6a38529a0dbe5c3a01cb4d429d987
György Hamar <gyurinak@gmail.com>
2016-11-29 11:16:58 +0100

    adding more test functions, refactor

--- test/com/codecool/shop/dao/ProductCategoryDaoTest.java
+++ test/com/codecool/shop/dao/ProductCategoryDaoTest.java
@@ -18,14 +18,22 @@ public class ProductCategoryDaoTest {
     private ProductCategory productCategory1;
     private ProductCategory productCategory2;
     private ProductCategoryDao productCategoryDao;
+    // virtual list of product categories to test against
+    private List<ProductCategory> testProductCategories;
 
     @Before
     public void setUp() throws Exception {
-        productCategory1 = new ProductCategory("name1", "department1", "description1");
-        productCategory2 = new ProductCategory("name2", "department2", "description2");
         productCategoryDao = ProductCategoryDaoMem.getInstance();
+        testProductCategories = new ArrayList();
+
+        productCategory1 = new ProductCategory("name1", "department1", "description1");
         productCategoryDao.add(productCategory1);
+        testProductCategories.add(productCategory1);
+
+        productCategory2 = new ProductCategory("name2", "department2", "description2");
         productCategoryDao.add(productCategory2);
+        testProductCategories.add(productCategory2);
+
     }
 
     @After
@@ -34,16 +42,30 @@ public class ProductCategoryDaoTest {
     }
 
     @Test
-    public void find() throws Exception {
+    public void TestFind() throws Exception {
         assertEquals(productCategoryDao.find(1), productCategory1);
     }
 
     @Test
-    public void getAll() throws Exception {
-        List<ProductCategory> productCategories = new ArrayList();
-        productCategories.add(productCategory1);
-        productCategoryDao.remove(2);
-        assertEquals(productCategoryDao.getAll(), productCategories);
+    public void TestFindInvalidID() throws Exception {
+        assertEquals(productCategoryDao.find(0), null);
     }
 
+    @Test
+    public void TestGetAll() throws Exception {
+        assertEquals(productCategoryDao.getAll(), testProductCategories);
+    }
+
+    @Test
+    public void TestRemove() throws Exception {
+        productCategoryDao.remove(2);
+        assertEquals(productCategoryDao.find(2), null);
+    }
+
+    @Test
+    // getAll() should give the same result after removing a category with an invalid ID
+    public void TestRemoveInvalidID() throws Exception {
+        productCategoryDao.remove(0);
+        assertEquals(productCategoryDao.getAll(), testProductCategories);
+    }
 }
