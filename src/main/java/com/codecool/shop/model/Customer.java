e82fc8358746a609e480e064c7cfb418072d4d82
berloc <berloc1987@gmail.com>
2016-12-13 10:47:27 +0100

    created empty Customer class

---
+++ src/main/java/com/codecool/shop/model/Customer.java
@@ -0,0 +1,51 @@
+package com.codecool.shop.model;
+
+/**
+ * Created by berloc on 2016.12.13..
+ */
+public class Customer {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int password;
+
+    public Customer(String firstName, String lastName, String email, int password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+}
