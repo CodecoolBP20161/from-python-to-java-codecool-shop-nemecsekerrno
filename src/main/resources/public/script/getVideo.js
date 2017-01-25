23e973c02df038579055a79d29864c52deba88b2
berloc <berloc1987@gmail.com>
2017-01-25 14:48:58 +0100

    final version

--- src/main/resources/public/script/getVideo.js
+++ src/main/resources/public/script/getVideo.js
@@ -3,38 +3,32 @@
  */
 
 $(document).ready(function() {
-    $('.youtube-icon').on('click', function() {
+    $('.youtube-icon').on('click', function () {
         sendData(this);
-    })
+    });
 });
 
-var sendData = function(button) {
-    console.log("sdéjklfélkasd");
-    $('.loading').show();
-    var prodId = $(button).data("prod-id");
-    var prodName = $().data("prod-name");
-    console.log(prodName);
-    $('.modal-header').html("Review of " + prodName);
-    $.ajax({
-        url: '/getvideo?id=' + prodId,
-        method: 'GET',
-        dataType: "text",
-        success: function (data) {
-            $('.loading').hide();
-            $('.modal-body').html(data);
-            $('.modal-close').on('click', function () {
-                stopVideo($('.video'));
-            });
-        }
-    })
+    var sendData = function (button) {
+        $('.loading').show();
+        var prodId = $(button).data("prod-id");
+        var prodName = $(button).data("prod-name");
+        $('.modal-header').html("Review of " + prodName);
+        $.ajax({
+            url: '/getvideo?id=' + prodId,
+            method: 'GET',
+            dataType: "text",
+            success: function (data) {
+                $('.loading').hide();
+                $('.video-body').html(data);
+            },
+            error: function(request, errorType, errorMessage) {
+                alert('Error: ' + errorMessage);
+            },
+            complete: function() {
+                $('.modal-close').on('click', function () {
+                    $('iframe').remove();
+                });
+            }
+        });
 
-};
-
-var stopVideo = function(player) {
-    var vidSrc = player.prop('src');
-    player.prop('src', ''); // to force it to pause
-    player.prop('src', vidSrc);
-};
-
-
-
+    };
