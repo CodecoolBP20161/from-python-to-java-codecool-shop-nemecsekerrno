8cf868a243d214aa9e4765167e394bb421c20b33
rekan <rekanagy06@gmail.com>
2017-01-26 23:50:33 +0100

    Completed update cart

--- src/main/resources/public/script/getShippingOptions.js
+++ src/main/resources/public/script/getShippingOptions.js
@@ -37,26 +37,26 @@ var sendToServer = function() {
 };
 
 var updateCart = function(cost) {
-    $(".shipping-cost").html(cost);
+    window.location = "http://localhost:8888/cart/reviewcomplete?shippingcost=" + cost;
 };
 
 var chooseOption = function(data) {
-    $("#express").html("<input type=\"radio\" name=\"optradio\" value=" + data.expressCourier.cost + data.expressCourier.currency +">" +
+    $("#express").html("<input type=\"radio\" name=\"optradio\" value=" + data.expressCourier.cost +">" +
         data.expressCourier.details + " - " +
         data.expressCourier.timeInHours + " hours - " +
         data.expressCourier.cost + " " + data.expressCourier.currency +
         "</input>");
-    $("#truck").html("<input type=\"radio\" name=\"optradio\" value=" + data.truck.cost + data.truck.currency + ">" +
+    $("#truck").html("<input type=\"radio\" name=\"optradio\" value=" + data.truck.cost + ">" +
         data.truck.details + " - " +
         data.truck.timeInHours + " hours - " +
         data.truck.cost + " " + data.truck.currency +
         "</input>");
-    $("#truckhighway").html("<input type=\"radio\" name=\"optradio\" value=" + data.truckViaHighway.cost + data.truckViaHighway.currency + ">" +
+    $("#truckhighway").html("<input type=\"radio\" name=\"optradio\" value=" + data.truckViaHighway.cost + ">" +
         data.truckViaHighway.details + " - " +
         data.truckViaHighway.timeInHours + " hours - " +
         data.truckViaHighway.cost + " " + data.truckViaHighway.currency +
         "</input>");
-    $("#timeMachine").html("<input type=\"radio\" name=\"optradio\" value=" + data.timeMachine.cost + data.timeMachine.currency + ">" +
+    $("#timeMachine").html("<input type=\"radio\" name=\"optradio\" value=" + data.timeMachine.cost + ">" +
         data.timeMachine.details + " - " +
         data.timeMachine.timeInHours + " hours - " +
         data.timeMachine.cost + " " + data.timeMachine.currency +
