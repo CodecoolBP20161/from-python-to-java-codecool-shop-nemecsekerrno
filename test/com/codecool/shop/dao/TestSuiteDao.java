74dda94920002c574213d315a37e445dfa44379b
rekan <rekanagy06@gmail.com>
2017-01-26 15:19:05 +0100

    Created test suites for Dao and DaoJdbc

---
+++ test/com/codecool/shop/dao/TestSuiteDao.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.dao;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ProductCategoryDaoTest.class, ProductDaoTest.class, SupplierDaoTest.class})
+public class TestSuiteDao {
+    //nothing
+}
---
+++ test/com/codecool/shop/dao/TestSuiteDaoJdbc.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.dao;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ProductCategoryDaoJdbcTest.class, ProductDaoJdbcTest.class, SupplierDaoJdbcTest.class})
+public class TestSuiteDaoJdbc {
+    //nothing
+}
