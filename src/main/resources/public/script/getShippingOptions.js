967d5f8a54f12c63ff245a112527949c2bb6e4af
berloc <berloc1987@gmail.com>
2017-01-26 07:47:26 +0100

    basic ajax we got the shippingoptoins json

--- src/main/resources/public/script/getShippingOptions.js
+++ src/main/resources/public/script/getShippingOptions.js
@@ -3,12 +3,26 @@
  */
 $(document).ready(function(){
    $("#calculate-shipping").click(function() {
-       var userInput = $("#user-address").val();
-       console.log(userInput);
+       sendToServer();
         // sendToServer($('#user-address').value)
    });
 });
 
-var sendToServer = function(address) {
+var sendToServer = function() {
+    var address = $("#user-address").val();
+    console.log(address);
+    $.ajax({
+        url: '/getshippingcost?address=' + address,
+        method: 'GET',
+        dataType: 'json',
+        success: function (data) {
+            console.log("kiskutyafasza");
+            console.log(data);
+            console.log(JSON.stringify(data));
+        },
+        // error: function(request, errorType, errorMessage) {
+        //     alert('Error: ' + errorMessage);
+        // }
+    })
 
-}
+};
