be78eae61273cfaa4275715e14a7d938adfd03bc
rekan <rekanagy06@gmail.com>
2017-01-26 11:33:46 +0100

    Implemented modal details and shippingcost js

--- src/main/resources/public/script/getShippingOptions.js
+++ src/main/resources/public/script/getShippingOptions.js
@@ -3,6 +3,7 @@
  */
 $(document).ready(function(){
    $("#calculate-shipping").click(function() {
+       $("label").empty();
        sendToServer();
    });
 });
@@ -17,15 +18,26 @@ var sendToServer = function() {
         method: 'GET',
         dataType: 'json',
         success: function (data) {
-            console.log(data);
-            console.log(data.expressCourier);
-            console.log(data.truck);
-            console.log(data.truckViaHighway);
-            console.log(data.timeMachine);
-            console.log(data.timeMachine.details)
+            $("#express").html("<input type=\"radio\" name=\"optradio\">" +
+                data.expressCourier.details + " - " +
+                data.expressCourier.timeInHours + " hours - " +
+                data.expressCourier.cost + " " + data.expressCourier.currency +
+                "</input>");
+            $("#truck").html("<input type=\"radio\" name=\"optradio\">" +
+                data.truck.details + " - " +
+                data.truck.timeInHours + " hours - " +
+                data.truck.cost + " " + data.truck.currency +
+                "</input>");
+            $("#truckhighway").html("<input type=\"radio\" name=\"optradio\">" +
+                data.truckViaHighway.details + " - " +
+                data.truckViaHighway.timeInHours + " hours - " +
+                data.truckViaHighway.cost + " " + data.truckViaHighway.currency +
+                "</input>");
+            $("#timeMachine").html("<input type=\"radio\" name=\"optradio\">" +
+                data.timeMachine.details + " - " +
+                data.timeMachine.timeInHours + " hours - " +
+                data.timeMachine.cost + " " + data.timeMachine.currency +
+                "</input>");
         }
-        // error: function(request, errorType, errorMessage) {
-        //     alert('Error: ' + errorMessage);
-        // }
     })
 };
