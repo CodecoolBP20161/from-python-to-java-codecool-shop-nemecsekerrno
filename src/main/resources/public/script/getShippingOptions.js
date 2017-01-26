39c64facb1eb229c1385708d2f4a4c0e1f20de8e
berloc <berloc1987@gmail.com>
2017-01-26 12:14:57 +0100

    create function and organized the code into that

--- src/main/resources/public/script/getShippingOptions.js
+++ src/main/resources/public/script/getShippingOptions.js
@@ -22,30 +22,34 @@ var sendToServer = function() {
         method: 'GET',
         dataType: 'json',
         success: function (data) {
-            $("#express").html("<input type=\"radio\" name=\"optradio\" value=" + data.expressCourier.cost + data.expressCourier.currency +">" +
-                data.expressCourier.details + " - " +
-                data.expressCourier.timeInHours + " hours - " +
-                data.expressCourier.cost + " " + data.expressCourier.currency +
-                "</input>");
-            $("#truck").html("<input type=\"radio\" name=\"optradio\" value=" + data.truck.cost + data.truck.currency + ">" +
-                data.truck.details + " - " +
-                data.truck.timeInHours + " hours - " +
-                data.truck.cost + " " + data.truck.currency +
-                "</input>");
-            $("#truckhighway").html("<input type=\"radio\" name=\"optradio\" value=" + data.truckViaHighway.cost + data.truckViaHighway.currency + ">" +
-                data.truckViaHighway.details + " - " +
-                data.truckViaHighway.timeInHours + " hours - " +
-                data.truckViaHighway.cost + " " + data.truckViaHighway.currency +
-                "</input>");
-            $("#timeMachine").html("<input type=\"radio\" name=\"optradio\" value=" + data.timeMachine.cost + data.timeMachine.currency + ">" +
-                data.timeMachine.details + " - " +
-                data.timeMachine.timeInHours + " hours - " +
-                data.timeMachine.cost + " " + data.timeMachine.currency +
-                "</input>");
+            chooseOption(data);
         }
     })
 };
 
 var updateCart = function(cost) {
     $(".shipping-cost").html(cost);
+};
+
+var chooseOption = function(data) {
+    $("#express").html("<input type=\"radio\" name=\"optradio\" value=" + data.expressCourier.cost + data.expressCourier.currency +">" +
+        data.expressCourier.details + " - " +
+        data.expressCourier.timeInHours + " hours - " +
+        data.expressCourier.cost + " " + data.expressCourier.currency +
+        "</input>");
+    $("#truck").html("<input type=\"radio\" name=\"optradio\" value=" + data.truck.cost + data.truck.currency + ">" +
+        data.truck.details + " - " +
+        data.truck.timeInHours + " hours - " +
+        data.truck.cost + " " + data.truck.currency +
+        "</input>");
+    $("#truckhighway").html("<input type=\"radio\" name=\"optradio\" value=" + data.truckViaHighway.cost + data.truckViaHighway.currency + ">" +
+        data.truckViaHighway.details + " - " +
+        data.truckViaHighway.timeInHours + " hours - " +
+        data.truckViaHighway.cost + " " + data.truckViaHighway.currency +
+        "</input>");
+    $("#timeMachine").html("<input type=\"radio\" name=\"optradio\" value=" + data.timeMachine.cost + data.timeMachine.currency + ">" +
+        data.timeMachine.details + " - " +
+        data.timeMachine.timeInHours + " hours - " +
+        data.timeMachine.cost + " " + data.timeMachine.currency +
+        "</input>");
 };
